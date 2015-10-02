90a61a19258c9c83ba7366c107c853ea6c4c69fd
Mark Miller <mark.kilpatrick.miller@gmail.com>
2015-10-02 18:36:18 -0400

    add login post controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,16 @@ class SessionsController < ApplicationController
 	def new 
 		@user = User.new
 	end
+
+	def create
+		user = User.find_by(username: params[:user][:username])
+    if user
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      flash[:invalid_login] = "Unable to find password & username combination"
+      redirect_to '/users/new'
+    end
+
+	end
 end
