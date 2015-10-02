a64ba6e7ce1dd703982efce7ff32c153a4402ae9
Mark Miller <mark.kilpatrick.miller@gmail.com>
2015-10-02 18:24:42 -0400

    fix root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  root 'question#index'
+  root 'questions#index'
   resource :questions
   resource :users
 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy', :via => :delete
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
