3ce761be1646bdbdefafe3ee18e945b3e36ec027
Mark Miller <mark.kilpatrick.miller@gmail.com>
2015-10-03 14:54:36 -0400

    add password check

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 
 	def create
 		user = User.find_by(username: session_params[:username])
-    if user
+    if user.try(:authenticate, session_params[:password])
       session[:user_id] = user.id
       redirect_to root_path
     else
