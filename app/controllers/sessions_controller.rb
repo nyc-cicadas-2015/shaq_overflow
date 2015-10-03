a33332e4b43f43187593ddaa76407b39ac97d39e
Mark Miller <mark.kilpatrick.miller@gmail.com>
2015-10-02 23:09:19 -0400

    refactor sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to root_path
     else
-      flash[:invalid_login] = "Unable to find password & username combination"
+      flash[:error] = "Unable to find password & username combination"
       redirect_to root_new_users
     end
 	end
