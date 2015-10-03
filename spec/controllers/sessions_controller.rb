17833a819492b5ee755992ba35b6fbd410f1e8c5
Mark Miller <mark.kilpatrick.miller@gmail.com>
2015-10-03 18:10:08 -0400

    add successful and unsuccesful options for GET #create

--- spec/controllers/sessions_controller.rb
+++ spec/controllers/sessions_controller.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 describe SessionsController do
+	describe "GET #create" do
+		describe "When succesful" do
+			
+		end
 
+		describe "When unsuccesful" do
+
+		end
+	end
 end
