57b3f860a1f333753fd65bbe7ff027a003c5d33b
Mark Miller <mark.kilpatrick.miller@gmail.com>
2015-10-02 23:08:27 -0400

    built functional user registration route + path + view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,14 @@ class UsersController < ApplicationController
   end
 
   def create
+  	user = User.new(user_params)
+  	if user.save
+  		session[:user_id] = user.id
+  		redirect_to user_path(user)
+  	else
+  		flash[:error] = "Invalid registration field"
+  		redirect_to new_user_path
+  	end
   end
 
   private
