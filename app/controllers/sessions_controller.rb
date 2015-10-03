e79885e7f6e252acb9d0a265f42501cee5b78160
Mark Miller <mark.kilpatrick.miller@gmail.com>
2015-10-03 00:44:56 -0400

    add logout path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,7 +14,8 @@ class SessionsController < ApplicationController
     end
 	end
 
-  def show
-    @user = User.find(params[:id])
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
   end
 end
