520460bb5b33b10e3a09389bf921958f3670d923
Mark Miller <mark.kilpatrick.miller@gmail.com>
2015-10-03 18:19:20 -0400

    succesful login redirect path built

--- spec/controllers/sessions_controller.rb
+++ spec/controllers/sessions_controller.rb
@@ -1,9 +1,20 @@
 require 'rails_helper'
 
 describe SessionsController do
-	describe "GET #create" do
+	describe "POST #create" do
+
 		describe "When succesful" do
-			
+			before(:each) {
+        user = create(:user)
+        post :create, session: { username: user[:username], password: user[:password] }
+      }
+      let(:user) { attributes_for(:user) }
+
+      it "redirects homepage" do 
+      	expect(response).to redirect_to(root_path)
+      end
+      # succesful login redirect path built
+
 		end
 
 		describe "When unsuccesful" do
