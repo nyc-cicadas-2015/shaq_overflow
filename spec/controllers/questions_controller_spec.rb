de139cb6c55c38aee9ddc30abf345e887d56b7d4
Navraj Nat <nnat425@gmail.com>
2015-10-03 15:14:43 -0400

    testing post create

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,9 +1,50 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# describe QuestionsController do
+describe QuestionsController do
 
-# describe "GET #new" do
+let(:log_in) {
+        user = create(:user)
+        post :create, :params  { username: user[:username], password: user[:password] }
+      }
 
-# end
+  describe "GET index" do
+    it "assigns @questions" do
+      question = create(:question)
+      get :index
+      expect(assigns(:questions)).to eq([question])
+    end
 
-# end
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+  end
+
+  describe "GET new" do
+    it "renders the new template" do
+      get :new
+      expect(response).to render_template("new")
+    end
+  end
+
+  describe "GET show" do
+    it "renders the show template" do
+      question = create(:question)
+      get :show, id: question.id
+      expect(response).to render_template("show")
+    end
+  end
+
+  describe "POST create" do
+   context "with valid attributes" do
+    it "saves the new contact in the database" do
+      expect{
+        log_in
+        post :create, question:attributes_for(:question)
+        }.to change{Question.count}.by(1)
+      end
+    end
+  end
+
+
+end
