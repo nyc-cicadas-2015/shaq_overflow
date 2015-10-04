0e4db0db6f310e0132bb6b177d50e07466e66cac
Jenny Lee <leejenny0403@gmail.com>
2015-10-04 14:29:47 -0400

    move question creation out of log me in method

--- spec/controllers/responses_controller_spec.rb
+++ spec/controllers/responses_controller_spec.rb
@@ -1,30 +1,31 @@
-# require "rails_helper"
+require "rails_helper"
 
-# describe ResponsesController do
-#   let(:log_me_in) {
-#     @user = create(:user)
-#     session[:user_id] = @user.id
-#     @question = create(:question)
-#   }
-#   let(:response_attr) { attributes_for(:response) }
+describe ResponsesController do
+  let(:log_me_in) {
+    @user = create(:user)
+    session[:user_id] = @user.id
+  }
+  let(:@question) = create(:question)
+  let(:response_attr) { attributes_for(:response) }
 
-#   describe "POST #create" do
-#     context "valid attributes" do
-#       it "creates new response" do
-#         log_me_in
-#         expect {
-#           post :create, response: { body: response[:body], respondable_id: @question.id, respondable_type: @question.class, user_id: response[:user] }
-#         }.to change(Response,:count).by(1)
-#       end
-#     end
+  describe "POST #create" do
+    context "valid attributes" do
+      it "creates new response" do
+        log_me_in
+        expect {
+          post :create, response: { body: response[:body], respondable_id: @question.id, respondable_type: @question.class, user_id: response[:user] }
+        }.to change(Response,:count).by(1)
+      end
+    end
 
-#     context "invalid attributes" do
-#       it "does not create response if field empty" do
-#         expect {
-#           post :create, response: { body: "", respondable_id: @question.id, respondable_type: @question.class, user_id: response[:user] }
-#           }.to_not change(Response,:count)
-#       end
-#     end
-#   end
+    context "invalid attributes" do
+      it "does not create response if field empty" do
+        log_me_in
+        expect {
+          post :create, response: { body: "", respondable_id: @question.id, respondable_type: @question.class, user_id: response[:user] }
+          }.to_not change(Response,:count)
+      end
+    end
+  end
 
-# end
+end
