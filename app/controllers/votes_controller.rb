fc5df39bbc1f546e11f92361f104d2a48c5061db
Mark Miller <mark.kilpatrick.miller@gmail.com>
2015-10-03 20:49:34 -0400

    add logic to make new question vote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,4 +2,16 @@ class VotesController < ApplicationController
 	def new
 		@vote = Vote.new
 	end
+
+	def create
+		if vote_params[:question_id] != nil
+			@question = Question.find_by(id: vote_params[:question_id])
+			binding.pry
+		end
+	end
+
+	private
+	def vote_params
+		params.require(:vote).permit(:question_id, :comment_id, :response_id)
+	end
 end
