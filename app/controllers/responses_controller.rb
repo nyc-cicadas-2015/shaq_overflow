f9ac11c9ccd48f586c92b32546fb38b3de6a8d91
Navraj Nat <nnat425@gmail.com>
2015-10-03 20:36:04 -0400

    link to new response added

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,5 +1,29 @@
 class ResponsesController < ApplicationController
 
+  def new
+    @response = Response.new
+  end
 
+
+  def create
+    if logged_in?
+      user = current_user
+      question = Question.find_by(id: params[:response][:question_id])
+      if params[:response][:body] == ""
+        flash[:error] = "Response field cannot be empty"
+      else
+        user.responses.create(params_response)
+      end
+      redirect_to question_path(question)
+    else
+      redirect_to login_path
+    end
+  end
+
+  private
+
+  def params_answer
+    params.require(:response).permit(:body, :question_id)
+  end
 
 end
--- spec/features/response_spec.rb
+++ spec/features/response_spec.rb
@@ -19,6 +19,7 @@ describe 'response form' do
   it "to see if it is on the page" do
     question = create(:question)
     visit question_path(question)
+    click_link "Comment"
     within("#new_response") do
       fill_in 'response_body', :with => response_attr[:body]
     end
