d4602cf4017606dd2bbbccec042d4fdb73027fac
Mark Miller <mark.kilpatrick.miller@gmail.com>
2015-10-04 18:32:03 -0400

    add MVP tag field in posts

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,11 +3,16 @@ class Tag < ActiveRecord::Base
   has_many :questions, through: :question_tags
 
   def self.make_tags(tags, question)
-    tag_array = tags.split
+    tag_array = tags.split(" ")
     tag_array.each do |tag|
-    	# check if tag already exists
-      question.tags.create(tag: tag)
+      question.tags.create(name: tag)
     end
   end
-
 end
+
+
+# tags = tag_params[:name]
+# thetags = tags.split(" ")
+# thetags.each do |tt|
+# 	question.tags.create(name: tt)
+# end
