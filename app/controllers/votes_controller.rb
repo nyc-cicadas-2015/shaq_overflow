c324ca5cda596815f469894e5a30c4a73640ecaa
Mark Miller <mark.kilpatrick.miller@gmail.com>
2015-10-03 22:04:44 -0400

    add VIP create vote route

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,9 +4,23 @@ class VotesController < ApplicationController
 	end
 
 	def create
-		if vote_params[:question_id] != nil
-			@question = Question.find_by(id: vote_params[:question_id])
-			binding.pry
+		if vote_params[:question_id] != nil			
+			question = Question.find_by(id: vote_params[:question_id])
+			if question.votes.where({user_id: session[:user_id]}) == []
+				upvote = question.votes.build(user_id: session[:user_id])
+				if upvote.save
+					flash[:message] = "Thanks for liking the question"
+				end
+			else
+				question = question.votes.find_by(user_id: session[:user_id])
+				question.destroy
+				flash[:message] = "You've unliked the post"
+			end
+		redirect_to root_path
+		elsif vote_params[:comment_id] != nil	
+			# logic goes here
+		elsif vote_params[:response_id] != nil	
+			# logic goes here
 		end
 	end
 
