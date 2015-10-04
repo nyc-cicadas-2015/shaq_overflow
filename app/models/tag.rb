93f2bdbed1b82c90e5b6de3b2917b06d9c943e39
Mark Miller <mark.kilpatrick.miller@gmail.com>
2015-10-04 18:19:51 -0400

    add tag parser to tag model

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,12 @@ class Tag < ActiveRecord::Base
   has_many :question_tags
   has_many :questions, through: :question_tags
 
+  def self.make_tags(tags, question)
+    tag_array = tags.split
+    tag_array.each do |tag|
+    	# check if tag already exists
+      question.tags.create(tag: tag)
+    end
+  end
+
 end
