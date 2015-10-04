c6264efaba333076734eaf3a72013196e6da24e8
Mark Miller <mark.kilpatrick.miller@gmail.com>
2015-10-03 22:49:48 -0400

    add vote logic for comment and response

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -7,22 +7,36 @@ class VotesController < ApplicationController
 		if logged_in?
 			if vote_params[:question_id] != nil			
 				question = Question.find_by(id: vote_params[:question_id])
-				if question.votes.where({user_id: session[:user_id]}) == []
-					upvote = question.votes.build(user_id: session[:user_id])
-					flash[:message] = "Thanks for liking the question" if upvote.save
+				if question.votes.where({user_id: session[:user_id]}).count == 0
+					question.votes.create(user_id: session[:user_id])
+					flash[:message] = "Thanks for liking the question"
+				else
+					question.votes.find_by(user_id: session[:user_id]).destroy
+					flash[:message] = "You've unliked the question"
+				end
+			elsif vote_params[:comment_id] != nil	
+				comment = Comment.find_by(id: vote_params[:question_id])
+				if comment.votes.where({user_id: session[:user_id]}).count == 0
+					comment.votes.create(user_id: session[:user_id])
+					flash[:message] = "Thanks for liking the question"
+				else
+					comment.votes.find_by(user_id: session[:user_id]).destroy
+					flash[:message] = "You've unliked the comment"
+				end			
+			elsif vote_params[:response_id] != nil	
+				response = Response.find_by(id: vote_params[:response_id])
+				if response.votes.where({user_id: session[:user_id]}).count == 0
+					response.votes.create(user_id: session[:user_id])
+					flash[:message] = "Thanks for liking the response"
 				else
 					question.votes.find_by(user_id: session[:user_id]).destroy
 					flash[:message] = "You've unliked the post"
 				end
-			redirect_to root_path
-			elsif vote_params[:comment_id] != nil	
-				# logic goes here
-			elsif vote_params[:response_id] != nil	
-				# logic goes here
 			end
 		else
 			redirect_to login_path
 		end
+		redirect_to root_path
 	end
 
 	private
