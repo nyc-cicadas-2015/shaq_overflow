f6ad39b78a22e05261c195e90ea57984874702d8
Mark Miller <mark.kilpatrick.miller@gmail.com>
2015-10-03 23:03:41 -0400

    add basic template to allow for question feature testing

--- spec/features/vote_spec.rb
+++ spec/features/vote_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
-describe 'Voting process' do
+describe 'Question Voting process' do
+	before(:each){
+		log_me_in
+	}
 
+	let(:log_me_in) {
+    user = create(:user)
+    visit login_path
+    within("#login") do
+      fill_in 'Username', :with => user.username
+      fill_in 'Password', :with => user.password
+      click_button 'Login'
+    end
+  }
+  it "to see upvote button on page" do
+
+  end
+  
+	# describe 'where user has NOT previously voted' do
+	# end
+	# describe 'that HAS previously voted' do
+	# end
 end
