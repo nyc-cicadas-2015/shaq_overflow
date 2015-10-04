4d7474531df5a72eb6233449ccb946f7f662ad3f
Mark Miller <mark.kilpatrick.miller@gmail.com>
2015-10-04 00:37:20 -0400

    change logic to reflect current votes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -14,15 +14,19 @@ class VotesController < ApplicationController
 					question.votes.find_by(user_id: session[:user_id]).destroy
 					flash[:message] = "You've unliked the question"
 				end
-			elsif vote_params[:comment_id] != nil	
-				comment = Comment.find_by(id: vote_params[:question_id])
-				if comment.votes.where({user_id: session[:user_id]}).count == 0
-					comment.votes.create(user_id: session[:user_id])
-					flash[:message] = "Thanks for liking the question"
+				redirect_to root_path
+
+			elsif vote_params[:answer_id] != nil	
+				answer = Answer.find_by(id: vote_params[:answer_id])
+				if answer.votes.where({user_id: session[:user_id]}).count == 0
+					answer.votes.create(user_id: session[:user_id])
+					flash[:message] = "Thanks for liking the answer"
 				else
-					comment.votes.find_by(user_id: session[:user_id]).destroy
-					flash[:message] = "You've unliked the comment"
-				end			
+					answer.votes.find_by(user_id: session[:user_id]).destroy
+					flash[:message] = "You've unliked the answer"
+				end
+				redirect_to root_path #need to change this to question path
+
 			elsif vote_params[:response_id] != nil	
 				response = Response.find_by(id: vote_params[:response_id])
 				if response.votes.where({user_id: session[:user_id]}).count == 0
@@ -36,11 +40,10 @@ class VotesController < ApplicationController
 		else
 			redirect_to login_path
 		end
-		redirect_to root_path
 	end
 
 	private
 	def vote_params
-		params.require(:vote).permit(:question_id, :comment_id, :response_id)
+		params.require(:vote).permit(:question_id, :answer_id, :response_id)
 	end
 end
