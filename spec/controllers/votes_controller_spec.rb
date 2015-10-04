9f9978fe41dc6d93fb93c9fcece157c143080fe9
Mark Miller <mark.kilpatrick.miller@gmail.com>
2015-10-03 22:56:42 -0400

    remove note from spec file

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -3,7 +3,3 @@ require 'rails_helper'
 describe SessionsController do
 
 end
-
-# +<%= form_for [@question,@response] , html: {id:"new_response"} do |f| %>
-# +<%= f.hidden_field :respondable_id, value: params[:question_id] %>
-# +<%= f.hidden_field :respondable_type, value: "Question" %>
