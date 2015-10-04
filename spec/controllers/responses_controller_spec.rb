9cf09378ca5353583b420c9e36affc5b5e5cf96d
Jenny Lee <leejenny0403@gmail.com>
2015-10-04 15:08:38 -0400

    pass response controller test for edit method

--- spec/controllers/responses_controller_spec.rb
+++ spec/controllers/responses_controller_spec.rb
@@ -5,8 +5,12 @@ describe ResponsesController do
     @user = create(:user)
     session[:user_id] = @user.id
   }
-  let(:@question) { create(:question) }
   let(:response_attr) { attributes_for(:response) }
+  let(:create_response) {
+    @question = create(:question)
+    @test_response = Response.create(body: response_attr[:body], respondable_id: @question.id, respondable_type: @question.class, user_id: @user.id)
+  }
+
 
   # describe "GET #new" do
   # end
@@ -35,7 +39,8 @@ describe ResponsesController do
 
     it "renders edit page" do
       log_me_in
-      get :edit
+      create_response
+      get :edit, id: @test_response.id
       response.should render_template :edit
     end
 
