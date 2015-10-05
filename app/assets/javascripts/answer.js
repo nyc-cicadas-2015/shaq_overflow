a1cc13c3ff708e2fed0dd768e4f34abb61cbd6b0
Jenny Lee <leejenny0403@gmail.com>
2015-10-05 00:30:40 -0400

    added answer holder div to question show page

--- app/assets/javascripts/answer.js
+++ app/assets/javascripts/answer.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-  $('')
+  $('#new_answer').on('submit', function(event) {
+    event.preventDefault();
+    console.log("ajax working");
+
+    $.ajax({
+      method: 'POST',
+      url: event.target.action,
+      data: $(event.target).serialize()
+    }).done(function(data) {
+      console.log("werks");
+
+    })
+  })
 })
