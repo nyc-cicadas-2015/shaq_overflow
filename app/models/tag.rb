a1c584ec6c8bc9f522c8ea2f0e43823f942bac28
Mark Miller <mark.kilpatrick.miller@gmail.com>
2015-10-04 21:17:43 -0400

    add tag validations

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,6 +2,8 @@ class Tag < ActiveRecord::Base
   has_many :question_tags
   has_many :questions, through: :question_tags
 
+  validates :name, presence: true
+
   def self.make_tags(tags, question)
     tag_array = tags.split(" ")
     tag_array.each do |new_tag|
