d07d8a468e6d5c3dc0c979a0122b96b41dd9bb4b
Mark Miller <mark.kilpatrick.miller@gmail.com>
2015-10-04 21:15:56 -0400

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,9 @@ class User < ActiveRecord::Base
   has_many :responses
   has_many :votes
 
+  validates :name, presence: true, uniqueness: true
+  validates :username, presence: true
+  validates :email, presence: true
+
   has_secure_password
 end
