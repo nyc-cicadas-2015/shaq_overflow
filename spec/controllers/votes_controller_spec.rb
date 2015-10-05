0984db7e856f27d34a6a3a59311c062694d72d81
Jenny Lee <leejenny0403@gmail.com>
2015-10-04 21:24:30 -0400

    comment out vote controller spec

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -1,5 +1,27 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-describe VotesController do
+# describe VotesController do
+#   let(:log_me_in) {
+#     @user = create(:user)
+#     session[:user_id] = @user.id
+#   }
+#   let(:create_question) { @question = create(:question) }
+#   let(:create_answer) { @answer = create(:answer) }
 
-end
+#   describe "POST #create" do
+#     before(:each) {
+#       log_me_in
+#     }
+#     context "correct subject" do
+#       it "upvotes question" do
+#         create_question
+#         expect {
+#           post :create,
+#         }
+
+#       end
+#     end
+
+#   end
+
+# end
